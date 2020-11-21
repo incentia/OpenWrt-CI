bcad64153d3e14cc077252114c97d7ff2ce4d3f3
8688Add <42204083+8688Add@users.noreply.github.com>
2020-11-22 05:14:07 +0800

    Add files via upload

---
+++ R619ac.sh
@@ -0,0 +1,26 @@
+#修改版本号
+#sed -i 's/OpenWrt/BGG $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+#使用源码自带ShadowSocksR Plus+出国软件
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.14/g' target/linux/ramips/Makefile
+#sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.14/g' target/linux/ramips/Makefile
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+#git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
+#git clone https://github.com/0118Add/luci-app-ssr-plus.git package/luci-app-ssr-plus
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall
+#svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package
+git clone https://github.com/xiaorouji/openwrt-package.git package/openwrt-package
+#git clone https://github.com/0118Add/luci-app-clash.git package/luci-app-clash
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+#git clone https://github.com/8688Add/small.git package/small
+git clone https://github.com/8688Add/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+#git clone https://github.com/8688Add/luci-theme-opentomato.git package/luci-theme-opentomato
+#git clone https://github.com/siropboy/mypackages.git package/luci-app-koolddns
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
