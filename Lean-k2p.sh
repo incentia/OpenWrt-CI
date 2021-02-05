3459070841d922cd49b22a54abb34a45bfd48545
8688Add <42204083+8688Add@users.noreply.github.com>
2021-02-06 01:01:26 +0800

    Update Lean-k2p.sh

--- Lean-k2p.sh
+++ Lean-k2p.sh
@@ -5,7 +5,7 @@
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 使用源码自带ShadowSocksR Plus+出国软件
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
@@ -23,11 +23,11 @@ git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app
 chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
 rm -rf package/lean/luci-app-unblockmusic
 svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/luci-app-unblockmusic package/lean/luci-app-unblockmusic
-#svn co https://github.com/Mattraks/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
-#svn co https://github.com/Mattraks/helloworld/trunk/tcping package/lean/tcping
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/tcping package/lean/tcping
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 sed -i 's/"ShadowSocksR Plus+"/"国际互联 Plus+"/g' package/lean/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-sed -i 's/"解锁网易云灰色歌曲"/"解锁网易Music"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+sed -i 's/"解锁网易云灰色歌曲"/"解锁网易 Music"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
